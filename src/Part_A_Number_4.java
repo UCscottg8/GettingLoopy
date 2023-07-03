e0fe16ee8346755648b1c5ace5928e638cb1db42
UCscottg8 <134948683+UCscottg8@users.noreply.github.com>
2023-07-03 10:18:11 -0400

    Ver 4

---
+++ src/Part_A_Number_4.java
@@ -0,0 +1,8 @@
+public class Part_A_Number_4 {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0; i -= 2) {
+            //Output statement
+            System.out.println(i);
+        }
+    }
+}
