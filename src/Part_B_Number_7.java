829fb7740c7c4a6ff5141419ee64df235ae2272b
UCscottg8 <134948683+UCscottg8@users.noreply.github.com>
2023-07-03 10:39:54 -0400

    Ver 7

---
+++ src/Part_B_Number_7.java
@@ -0,0 +1,11 @@
+public class Part_B_Number_7 {
+    public static void main(String[] args) {
+            for (int i = 1; i <= 5; i++) {
+                for (int j = 1; j <= 5; j++) {
+                    System.out.print("*");
+                }
+                //Output Statement
+                System.out.println();
+            }
+        }
+    }
