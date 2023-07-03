cf79e661dafe291cb3eafd904b1429c48abd5f37
UCscottg8 <134948683+UCscottg8@users.noreply.github.com>
2023-07-03 10:04:31 -0400

    Ver 3

---
+++ src/Part_A_Number_2.java
@@ -0,0 +1,8 @@
+public class Part_A_Number_2 {
+    public static void main(String[] args) {
+        for (int i = 30; i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
+
---
+++ src/Part_A_Number_3.java
@@ -0,0 +1,8 @@
+public class Part_A_Number_3 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 18; i += 3) {
+            //Output statement
+            System.out.println(i);
+        }
+    }
+}
