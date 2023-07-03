e5153099650616e4fa2e03277294122bbb4b43fd
UCscottg8 <134948683+UCscottg8@users.noreply.github.com>
2023-07-03 09:57:07 -0400

    Ver 2.5

--- src/Part_A_Number_1.java
+++ src/Part_A_Number_1.java
@@ -1,6 +1,7 @@
 public class Part_A_Number_1 {
     public static void main(String[] args) {
         for (int i = 0; i <= 30; i++) {
+            //Output statement
             System.out.println(i);
         }
     }
