f5b2100c1ba9320ee18264fae6ce727167ef2ad7
UCscottg8 <134948683+UCscottg8@users.noreply.github.com>
2023-07-03 09:33:00 -0400

    Ver 2

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 30; i++) {
+            //Output statement
+            System.out.println(i);
+        }
+    }
+}
